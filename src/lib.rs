7586fd392a3b0b021e9bb41956b96d5b4a485720
Christian Duerr <chris.durr@phylum.io>
2022-08-26 19:59:22 +0200

    Remove unnecessary main from doctest

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,18 @@
 //! use birdcage::{Birdcage, Sandbox};
 //! use tempfile::NamedTempFile;
 //!
-//! fn main() {
-//!     // Setup our test file.
-//!     let file = NamedTempFile::new().unwrap();
+//! // Setup our test file.
+//! let file = NamedTempFile::new().unwrap();
 //!
-//!     // Reads without sandbox work.
-//!     fs::read_to_string(file.path()).unwrap();
+//! // Reads without sandbox work.
+//! fs::read_to_string(file.path()).unwrap();
 //!
-//!     // Initialize the sandbox; by default everything is prohibited.
-//!     Birdcage::new().unwrap().lock().unwrap();
+//! // Initialize the sandbox; by default everything is prohibited.
+//! Birdcage::new().unwrap().lock().unwrap();
 //!
-//!     // Reads with sandbox should fail.
-//!     let result = fs::read_to_string(file.path());
-//!     assert!(result.is_err());
-//! }
+//! // Reads with sandbox should fail.
+//! let result = fs::read_to_string(file.path());
+//! assert!(result.is_err());
 //! ```
 
 use std::path::PathBuf;
