63c8c27f09ff0f9911175c783b60d7f5ae2c27a1
Andrea Venuta <venutawebdesign@gmail.com>
2022-09-13 16:13:30 +0200

    Net tests

---
+++ tests/net.rs
@@ -0,0 +1,24 @@
+use std::net::{TcpListener, TcpStream};
+
+use birdcage::{Birdcage, Exception, Sandbox};
+
+#[test]
+fn it_blocks_network() {
+    let listener = TcpListener::bind("127.0.0.1:31337");
+    assert!(listener.is_ok());
+    drop(listener);
+
+    let stream = TcpStream::connect("www.google.com:443");
+    assert!(stream.is_ok());
+    drop(stream);
+
+    Birdcage::new().unwrap().lock().unwrap();
+
+    let listener = TcpListener::bind("127.0.0.1:31337");
+    assert!(listener.is_err());
+    drop(listener);
+
+    let stream = TcpStream::connect("www.google.com:443");
+    assert!(stream.is_err());
+    drop(stream);
+}
