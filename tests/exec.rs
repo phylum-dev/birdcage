e0ef3dc870e29689280b2ab3fc9dd29141e5d1ce
Andrea Venuta <venutawebdesign@gmail.com>
2022-09-14 15:41:21 +0200

    Changed exec test

--- tests/exec.rs
+++ tests/exec.rs
@@ -6,13 +6,12 @@ use birdcage::{Birdcage, Exception, Sandbox};
 #[test]
 fn execution() {
     let mut bc = Birdcage::new().unwrap();
-    bc.add_exception(Exception::ExecuteAndRead("/bin/ls".into())).unwrap();
-    bc.add_exception(Exception::Read("/tmp".into())).unwrap();
+    bc.add_exception(Exception::ExecuteAndRead("/bin/echo".into())).unwrap();
     bc.lock().unwrap();
 
-    let cmd = Command::new("/bin/ls").arg("/tmp").spawn();
+    let cmd = Command::new("/bin/echo").arg("hello world").spawn();
     assert!(cmd.is_ok());
 
-    let cmd_file = fs::read("/bin/ls");
+    let cmd_file = fs::read("/bin/echo");
     assert!(cmd_file.is_ok());
 }
