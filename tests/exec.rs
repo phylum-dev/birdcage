d874eb8f3fdd7a3d65aab19527e7f09bf35c60cb
Andrea Venuta <venutawebdesign@gmail.com>
2022-09-15 13:58:17 +0200

    Modified execution test

--- tests/exec.rs
+++ tests/exec.rs
@@ -6,13 +6,18 @@ use birdcage::{Birdcage, Exception, Sandbox};
 #[test]
 fn execution() {
     let mut bc = Birdcage::new().unwrap();
-    bc.add_exception(Exception::ExecuteAndRead("/bin/echo".into())).unwrap();
+    bc.add_exception(Exception::ExecuteAndRead("/bin/ls".into())).unwrap();
+    bc.add_exception(Exception::Read("/dev/null".into())).unwrap();
     bc.lock().unwrap();
 
-    let cmd = Command::new("/bin/echo").arg("hello world").status();
+    let cmd = Command::new("/bin/ls").arg("/dev/null").status().unwrap();
     println!("{:?}", cmd);
-    assert!(cmd.is_ok());
+    assert!(cmd.success());
 
-    let cmd_file = fs::read("/bin/echo");
+    let cmd = Command::new("/bin/ls").arg("/tmp/").status().unwrap();
+    println!("{:?}", cmd);
+    assert!(!cmd.success());
+
+    let cmd_file = fs::read("/bin/ls");
     assert!(cmd_file.is_ok());
 }
