b6c99a7ef2204c52fb7288547ac29a62d6e09bd3
Andrea Venuta <venutawebdesign@gmail.com>
2022-09-15 14:25:07 +0200

    Added non-symlink in test for osx

--- tests/exec.rs
+++ tests/exec.rs
@@ -7,14 +7,15 @@ use birdcage::{Birdcage, Exception, Sandbox};
 fn execution() {
     let mut bc = Birdcage::new().unwrap();
     bc.add_exception(Exception::ExecuteAndRead("/bin/ls".into())).unwrap();
-    bc.add_exception(Exception::Read("/dev/null".into())).unwrap();
+    bc.add_exception(Exception::Read("/tmp".into())).unwrap();
+    bc.add_exception(Exception::Read("/private/tmp".into())).unwrap();
     bc.lock().unwrap();
 
-    let cmd = Command::new("/bin/ls").arg("/dev/null").status().unwrap();
+    let cmd = Command::new("/bin/ls").arg("/tmp/").status().unwrap();
     println!("{:?}", cmd);
     assert!(cmd.success());
 
-    let cmd = Command::new("/bin/ls").arg("/tmp/").status().unwrap();
+    let cmd = Command::new("/bin/ls").arg("/dev/").status().unwrap();
     println!("{:?}", cmd);
     assert!(!cmd.success());
 
