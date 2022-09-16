da19a67c06c49efbf6babd5c462b3962ff37f93c
Andrea Venuta <venutawebdesign@gmail.com>
2022-09-16 15:24:33 +0200

    Removed url

--- tests/net.rs
+++ tests/net.rs
@@ -4,14 +4,10 @@ use birdcage::{Birdcage, Exception, Sandbox};
 
 #[test]
 fn network() {
-    let resolv_conf_path = std::fs::canonicalize("/etc/resolv.conf").unwrap();
-
     let mut bc = Birdcage::new().unwrap();
     bc.add_exception(Exception::Networking).unwrap();
-    bc.add_exception(Exception::Read(resolv_conf_path)).unwrap();
-    bc.add_exception(Exception::Read("/".into())).unwrap();
     bc.lock().unwrap();
 
-    TcpStream::connect("example.org:80").unwrap();
+    TcpStream::connect("8.8.8.8:443").unwrap();
     TcpListener::bind("127.0.0.1:31337").unwrap();
 }
