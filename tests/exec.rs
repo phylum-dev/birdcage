36d42379f60cf367f7bb72abeed3eae526653906
Andrea Venuta <andrea@phylum.io>
2022-09-19 17:26:29 +0200

    Fixed true test

--- tests/exec.rs
+++ tests/exec.rs
@@ -5,17 +5,16 @@ use birdcage::{Birdcage, Exception, Sandbox};
 
 #[test]
 fn execution() {
-    const TRUE_PATH: &str = if cfg!(target_os = "macos") { "/usr/bin/true" } else { "/bin/true" };
-
     let mut bc = Birdcage::new().unwrap();
-    bc.add_exception(Exception::ExecuteAndRead(TRUE_PATH.into())).unwrap();
+    bc.add_exception(Exception::ExecuteAndRead("/usr/bin/true".into())).unwrap();
+    bc.add_exception(Exception::ExecuteAndRead("/usr/lib".into())).unwrap();
     bc.lock().unwrap();
 
     // Check for success when executing `true`.
-    let cmd = Command::new(TRUE_PATH).status().unwrap();
+    let cmd = Command::new("/usr/bin/true").status().unwrap();
     assert!(cmd.success());
 
     // Check for success on reading the `true` file.
-    let cmd_file = fs::read(TRUE_PATH);
+    let cmd_file = fs::read("/usr/bin/true");
     assert!(cmd_file.is_ok());
 }
