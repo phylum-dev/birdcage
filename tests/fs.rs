36d28f03d84d809d92e1a0200eeb1f9d43741532
andreaphylum <68552656+andreaphylum@users.noreply.github.com>
2022-09-21 17:20:10 +0200

    Removed unnecessary canonicalizations (#9)
    Co-authored-by: Andrea Venuta <venutawebdesign@gmail.com>

--- tests/fs.rs
+++ tests/fs.rs
@@ -8,14 +8,14 @@ fn partial_fs() {
     const FILE_CONTENT: &str = "expected content";
 
     // Setup our test files.
-    let private_path = fs::canonicalize(NamedTempFile::new().unwrap()).unwrap();
+    let private_path = NamedTempFile::new().unwrap();
     fs::write(&private_path, FILE_CONTENT.as_bytes()).unwrap();
-    let public_path = fs::canonicalize(NamedTempFile::new().unwrap()).unwrap();
+    let public_path = NamedTempFile::new().unwrap();
     fs::write(&public_path, FILE_CONTENT.as_bytes()).unwrap();
 
     // Activate our sandbox.
     let mut birdcage = Birdcage::new().unwrap();
-    birdcage.add_exception(Exception::Read(public_path.clone())).unwrap();
+    birdcage.add_exception(Exception::Read(public_path.path().into())).unwrap();
     birdcage.lock().unwrap();
 
     // Access to the public file is allowed.
