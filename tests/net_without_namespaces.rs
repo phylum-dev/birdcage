4a9f20ab821cd4d167260f98e9de160f01cfb8d6
Christian Duerr <chris.durr@phylum.io>
2023-09-14 21:52:50 +0200

    Add tests for net filter without seccomp/namespace

---
+++ tests/net_without_namespaces.rs
@@ -0,0 +1,41 @@
+#[cfg(target_os = "linux")]
+use std::collections::BTreeMap;
+#[cfg(target_os = "linux")]
+use std::net::TcpStream;
+
+#[cfg(target_os = "linux")]
+use birdcage::{Birdcage, Sandbox};
+#[cfg(target_os = "linux")]
+use seccompiler::{BpfProgram, SeccompAction, SeccompFilter, TargetArch};
+
+#[cfg(target_os = "linux")]
+#[cfg(target_arch = "x86_64")]
+const ARCH: TargetArch = TargetArch::x86_64;
+#[cfg(target_os = "linux")]
+#[cfg(target_arch = "aarch64")]
+const ARCH: TargetArch = TargetArch::aarch64;
+
+#[cfg(target_os = "linux")]
+fn main() {
+    // Create seccomp filter blocking `unshare` syscall.
+    let mut rules = BTreeMap::new();
+    rules.insert(libc::SYS_unshare, Vec::new());
+    let filter = SeccompFilter::new(
+        rules,
+        SeccompAction::Allow,
+        SeccompAction::Errno(libc::EACCES as u32),
+        ARCH,
+    )
+    .unwrap();
+    let program: BpfProgram = filter.try_into().unwrap();
+    seccompiler::apply_filter(&program).unwrap();
+
+    let birdcage = Birdcage::new().unwrap();
+    birdcage.lock().unwrap();
+
+    let result = TcpStream::connect("8.8.8.8:443");
+    assert!(result.is_err());
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() {}
