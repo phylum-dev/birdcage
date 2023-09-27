3a51640aa8dafe65cd6a87a9820893b29e2a27f7
Christian Duerr <chris.durr@phylum.io>
2023-09-27 23:55:58 +0200

    Add test for ensuring consistent G/UID mappings

---
+++ tests/consistent_id_mappings.rs
@@ -0,0 +1,16 @@
+use birdcage::{Birdcage, Sandbox};
+
+fn main() {
+    let uid = unsafe { libc::getuid() };
+    let gid = unsafe { libc::getgid() };
+    let euid = unsafe { libc::geteuid() };
+    let egid = unsafe { libc::getegid() };
+
+    let birdcage = Birdcage::new().unwrap();
+    birdcage.lock().unwrap();
+
+    assert_eq!(uid, unsafe { libc::getuid() });
+    assert_eq!(gid, unsafe { libc::getgid() });
+    assert_eq!(euid, unsafe { libc::geteuid() });
+    assert_eq!(egid, unsafe { libc::getegid() });
+}
