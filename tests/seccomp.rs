233ba2973a3c6d821b64005656a7e0ae7074b8c6
Christian Duerr <chris.durr@phylum.io>
2023-10-06 20:33:55 +0200

    Extend seccomp tests

--- tests/seccomp.rs
+++ tests/seccomp.rs
@@ -1,4 +1,7 @@
 #[cfg(target_os = "linux")]
+use std::ffi::CString;
+
+#[cfg(target_os = "linux")]
 use birdcage::{Birdcage, Sandbox};
 
 #[cfg(target_os = "linux")]
@@ -6,8 +9,25 @@ fn main() {
     // Activate our sandbox.
     Birdcage::new().lock().unwrap();
 
+    // Ensure `chdir` is allowed.
+    let root_path = CString::new("/").unwrap();
+    let result = unsafe { libc::chdir(root_path.as_ptr()) };
+    assert_eq!(result, 0);
+
+    // Ensure `unshare` is always blocked.
     let result = unsafe { libc::unshare(libc::CLONE_NEWUSER) };
     assert_eq!(result, -1);
+
+    // Ensure `clone` is blocked with `CLONE_NEWUSER`.
+    let stack = unsafe { libc::malloc(4096) };
+    let flags = libc::CLONE_NEWUSER as libc::c_ulong;
+    let result = unsafe { libc::syscall(libc::SYS_clone, flags, stack) };
+    assert_eq!(result, -1);
+
+    // Ensure `clone` is allowed without flags.
+    let flags = 0;
+    let result = unsafe { libc::syscall(libc::SYS_clone, flags, stack) };
+    assert!(result > 0);
 }
 
 #[cfg(not(target_os = "linux"))]
