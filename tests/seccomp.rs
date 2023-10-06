a9d302e288370e32baf32d6992d03d4069f39028
Christian Duerr <chris.durr@phylum.io>
2023-10-06 23:30:37 +0200

    Remove successful clone test

--- tests/seccomp.rs
+++ tests/seccomp.rs
@@ -23,11 +23,6 @@ fn main() {
     let flags = libc::CLONE_NEWUSER as libc::c_ulong;
     let result = unsafe { libc::syscall(libc::SYS_clone, flags, stack) };
     assert_eq!(result, -1);
-
-    // Ensure `clone` is allowed without flags.
-    let flags = 0;
-    let result = unsafe { libc::syscall(libc::SYS_clone, flags, stack) };
-    assert!(result > 0);
 }
 
 #[cfg(not(target_os = "linux"))]
