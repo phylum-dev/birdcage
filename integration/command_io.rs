0fa3ac079befc8f597605051534f4184be8bd3a3
Christian Duerr <chris.durr@phylum.io>
2024-04-06 00:12:16 +0200

    Add test for signal handling

--- integration/command_io.rs
+++ integration/command_io.rs
@@ -1,9 +1,15 @@
 use std::io::Write;
+use std::os::unix::process::ExitStatusExt;
 
 use birdcage::process::{Command, Stdio};
 use birdcage::{Birdcage, Exception, Sandbox};
 
 fn main() {
+    pipe_stdin_to_stdout();
+    exit_signal();
+}
+
+fn pipe_stdin_to_stdout() {
     // Setup echo-back command.
     let mut cmd = Command::new("cat");
     cmd.stdin(Stdio::piped());
@@ -17,9 +23,27 @@ fn main() {
 
     // Write test data to stdin.
     let expected = b"test\n";
-    child.stdin.as_mut().unwrap().write(expected).unwrap();
+    let _ = child.stdin.as_mut().unwrap().write(expected).unwrap();
 
     // Read stdout.
     let output = child.wait_with_output().unwrap();
     assert_eq!(&output.stdout, expected);
+}
+
+fn exit_signal() {
+    // Setup echo-back command.
+    let cmd = Command::new("cat");
+
+    // Spawn sandbox child.
+    let mut sandbox = Birdcage::new();
+    sandbox.add_exception(Exception::ExecuteAndRead("/".into())).unwrap();
+    sandbox.add_exception(Exception::WriteAndRead("/".into())).unwrap();
+    let mut child = sandbox.spawn(cmd).unwrap();
+
+    // Kill the child.
+    child.kill().unwrap();
+
+    // Read stdout.
+    let status = child.wait().unwrap();
+    assert_eq!(status.signal(), Some(9));
 }
